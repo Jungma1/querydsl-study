a8af8408bb810739aca4aaf2db48410207124b2a
Jungma <mbjun3630@naver.com>
2022-08-27 22:35:13 +0900

    인터페이스 지원 - QuerydslPredicateExecutor

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 @SpringBootTest
@@ -91,5 +92,36 @@ class MemberRepositoryTest {
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username")
                 .containsExactly("member1", "member2", "member3");
+    }
+
+    /**
+     * 한계점)
+     * 1. 묵시적 조인은 가능하지만 left join 이 불가능
+     * 2. 클라이언트가 Querydsl 에 의존
+     * 3. 복잡한 실무환경에서 사용하기에는 한계가 명확
+     */
+    @Test
+    void querydslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> result = memberRepository.findAll(
+                member.age.between(10, 40).and(member.username.eq("member1")));
+
+        for (Member findMember : result) {
+            System.out.println("findMember = " + findMember);
+        }
     }
 }
